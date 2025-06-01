4e8bbeaadcc0c9be56a0c783a4f0bbf81c22b738
Jung Hyun Son <mrsono0@hotmail.com>
2025-06-01 15:01:31 +0900

    Update sommelier.py

--- sommelier.py
+++ sommelier.py
@@ -84,10 +84,11 @@ def describe_dish_flavor(image_bytes, query):
   return llm.invoke(messages).content
 
 def search_wine(dish_flavor):
-  results = vector_store.similarity_search(dish_flavor, k=2)
+  results = vector_store.similarity_search_with_score(dish_flavor, k=2)
   return {
     "dish_flavor" : dish_flavor,
-    "wine_reviews" : "\n\n".join([doc.page_content for doc in results])
+    # "wine_reviews" : "\n\n".join([doc.page_content for doc in results])
+    "wine_reviews" : "\n\n".join(["유사도 : " + str(round(doc[1] * 100, 3)) + "% \n\n" + doc[0].page_content for doc in results])
   }
 
 
